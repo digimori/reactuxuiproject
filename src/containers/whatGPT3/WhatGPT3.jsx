2fb53b657f457596aef03330571fcef5f195dc92
digimori <hanaorikuu@gmail.com>
2023-07-12 14:24:24 +0000

    Styles implemented for Features component - spacing and H1 colour/fonts

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -6,7 +6,7 @@ const WhatGPT3 = () => {
   return (
     <div className='gpt3__whatgpt3 section__margin' id="wgpt3">
       <div className='gpt3__whatgpt3-feature'>
-        <Feature />
+        <Feature title="What is GPT-3" text="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Natus distinctio nisi error eum, nihil optio explicabo unde nemo sint accusantium inventore, accusamus voluptates fugit repudiandae molestias ex corrupti vero nulla." />
       </div>
       <div className='gpt3__whatgpt3-heading'>
         <h1 className='gradient__text'>
@@ -15,9 +15,9 @@ const WhatGPT3 = () => {
         <p>Explore the library</p>
       </div>
       <div className='gpt3__whatgpt3-container'>
-        <Feature />
-        <Feature />
-        <Feature />
+        <Feature title="Chatbots" text="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Natus distinctio nisi error eum, nihil optio explicabo unde nemo sint accusantium inventore, accusamus voluptates fugit repudiandae molestias ex corrupti vero nulla." />
+        <Feature title="Knowledgebase" text="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Natus distinctio nisi error eum, nihil optio explicabo unde nemo sint accusantium inventore, accusamus voluptates fugit repudiandae molestias ex corrupti vero nulla." />
+        <Feature title="Education" text="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Natus distinctio nisi error eum, nihil optio explicabo unde nemo sint accusantium inventore, accusamus voluptates fugit repudiandae molestias ex corrupti vero nulla." />
       </div>
     </div>
   )

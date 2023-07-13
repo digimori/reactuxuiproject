1bb5ce2850afcc8819c3f14ca89fb5906b4a9671
digimori <hanaorikuu@gmail.com>
2023-07-13 13:05:08 +0000

    CTA section added with media queries - Might add some more for smaller screens, but it does seem to function fine with flex-direction column as is

--- src/components/cta/Cta.jsx
+++ src/components/cta/Cta.jsx
@@ -4,7 +4,13 @@ import './cta.css';
 const Cta = () => {
   return (
     <div className='gpt3__cta'>
-      
+      <div className='gpt3__cta-content'>
+        <p>Request early access to get started!</p>
+        <h3>Register today and start exploring endless possibilities.</h3>
+      </div>
+      <div className='gpt3__cta-btn'>
+        <button type='button'>Get Started</button>
+      </div>
     </div>
   )
 

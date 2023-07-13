a6ed2b27d48c96d661d9357b4bdbb829755780b4
digimori <hanaorikuu@gmail.com>
2023-07-13 12:32:53 +0000

    Base divs and content set for Possibility component - Styles to be added

--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
 import './possibility.css';
+import possibilityImage from '../../assets/possibility.png';
 
 const Possibility = () => {
   return (
-    <div>Possibility</div>
+    <div className='gpt3__possibility section__padding' id="possibility">
+      <div className='gpt3__possibility-image'>
+        <img src={possibilityImage} alt="possibility" />
+      </div>
+      <div className='gpt3__possibility-content'>
+        <h4>Request early access to get started</h4>
+        <h1 className='gradient-text'>The possibilities are beyond your imagination</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores deleniti fuga accusamus aperiam cupiditate nam quia, a placeat ex tempore molestias est eos cum aut vel repellat, modi nihil dignissimos.</p>
+      <h4>Request early access to get started!</h4>
+      </div>
+    </div>
   )
 }
 
